782f94b79d698758aef3a135c497f17bcd4e4d68
Keith L <k8t_loise16@yahoo.com>
2021-04-06 18:46:46 +1200

    Added MemberExtension

---
+++ app/src/Extensions/MemberExtension.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace {
+
+    use SilverStripe\AssetAdmin\Forms\UploadField;
+    use SilverStripe\Assets\Image;
+    use SilverStripe\Forms\CheckboxField;
+    use SilverStripe\Forms\FieldList;
+    use SilverStripe\Forms\GridField\GridField;
+    use SilverStripe\Forms\GridField\GridFieldConfig_RecordEditor;
+    use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
+    use SilverStripe\Forms\TextField;
+    use SilverStripe\ORM\DataExtension;
+    use Symbiote\GridFieldExtensions\GridFieldEditableColumns;
+    use UndefinedOffset\SortableGridField\Forms\GridFieldSortableRows;
+
+    class MemberExtension extends DataExtension
+    {
+        private static $db = [
+            'Position' => 'Text',
+            'Bio' => 'HTMLText',
+        ];
+
+        private static $has_one = [
+            'ProfileImage' => Image::class
+        ];
+
+        private static $has_many = [
+            'Socials' => Socials::class
+        ];
+
+        private static $owns = [
+            'ProfileImage'
+        ];
+
+        public function updateCMSFields(FieldList $fields)
+        {
+            $fields->addFieldToTab('Root.Main', TextField::create('Position'));
+            $fields->addFieldToTab('Root.Main', UploadField::create('ProfileImage')
+                ->setFolderName('ProfileImages'),'Locale');
+            $fields->addFieldToTab('Root.Main', HTMLEditorField::create('Bio'),'Locale');
+
+            $gridConfig = GridFieldConfig_RecordEditor::create(9999);
+            if ($this->owner->Socials()->Count()) {
+                $gridConfig->addComponent(new GridFieldSortableRows('Sort'));
+            }
+            $gridConfig->addComponent(new GridFieldEditableColumns());
+            $gridColumns = $gridConfig->getComponentByType(GridFieldEditableColumns::class);
+            $gridColumns->setDisplayFields([
+                'Archived' => [
+                    'title' => 'Archive',
+                    'callback' => function($record, $column, $grid) {
+                        return CheckboxField::create($column);
+                    }
+                ]
+            ]);
+
+            $gridField = GridField::create(
+                'Socials',
+                'Socials',
+                $this->owner->Socials(),
+                $gridConfig
+            );
+
+            $fields->addFieldToTab('Root.Socials', $gridField,'Locale');
+        }
+    }
+}
