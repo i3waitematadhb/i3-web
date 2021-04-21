2780298d5ac18f4cfe1da6ff4352d521888ba135
Keith L <k8t_loise16@yahoo.com>
2021-04-21 17:25:17 +1200

    Added Section - Image with text overlay

---
+++ app/src/Sections/ImageWithTextOverlay.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace {
+
+    use SilverStripe\AssetAdmin\Forms\UploadField;
+    use SilverStripe\Assets\Image;
+    use SilverStripe\Forms\CheckboxField;
+    use SilverStripe\Forms\DropdownField;
+    use SilverStripe\Forms\FieldList;
+    use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
+    use TractorCow\Colorpicker\Color;
+
+    class ImageWithTextOverlay extends Section
+    {
+        private static $singular_name = 'Image With Text Overlay';
+
+        private static $db = [
+            'Content'            => 'HTMLText',
+            'ContentPosition'    => 'Varchar',
+            'ContentBgColor'     => Color::class,
+        ];
+
+        private static $has_one = [
+            'Image' => Image::class
+        ];
+
+        private static $owns = [
+            'Image'
+        ];
+
+        public function getSectionCMSFields(FieldList $fields)
+        {
+            $fields->addFieldToTab('Root.Main', UploadField::create('Image')
+                ->setFolderName('ImageWithTextOverlay/Images'));
+            $fields->addFieldToTab('Root.Main', HTMLEditorField::create('Content'));
+            $fields->addFieldToTab('Root.Main', DropdownField::create('ContentPosition', 'Content position',
+                array(
+                    'position-left'  => 'Left',
+                    'position-right' => 'Right'
+                )
+            ));
+        }
+    }
+}
