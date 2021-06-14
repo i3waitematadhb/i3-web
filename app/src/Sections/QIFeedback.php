64574f072eb4641685926faff8510bc74dcd1e02
Keith L <k8t_loise16@yahoo.com>
2021-06-14 16:07:20 +1200

    Update QIFeedback

--- app/src/Sections/QIFeedback.php
+++ app/src/Sections/QIFeedback.php
@@ -2,8 +2,20 @@
 
 namespace {
 
+    use SilverStripe\Forms\FieldList;
+    use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
+
     class QIFeedback extends Section
     {
         private static $singular_name = 'QI Feedback';
+
+        private static $db = [
+            'Content' => 'HTMLText',
+        ];
+
+        public function getSectionCMSFields(FieldList $fields)
+        {
+            $fields->addFieldToTab('Root.Main', HTMLEditorField::create('Content'));
+        }
     }
 }
